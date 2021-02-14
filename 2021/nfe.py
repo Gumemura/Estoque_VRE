23996d453fb9393e26a9a51049bd1f6bdc854bc0
Gumemura <gumemura123@gmail.com>
2021-02-14 13:04:41 -0300

    automatizador de nfe

---
+++ 2021/nfe.py
@@ -0,0 +1,26 @@
+import xlrd
+
+f = open("items.xml", "w")
+
+# Give the location of the file
+loc = ("teste.xlsx")
+ 
+# To open Workbook
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+num_rows= sheet.nrows
+
+products = []
+
+for r in range(0, num_rows):
+    product = [sheet.cell_value(r, 0), sheet.cell_value(r, 1)]  
+    products.append(product)
+
+for elem in products:
+    print("Name: " + elem[0] + "\t| " + "Price: " + str(elem[1]))
+    f.write(elem[0] + " " + str(elem[1]) + "\n")
+
+
+
+f.close()
