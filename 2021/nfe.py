4f131d281300ffe19a07188258ffd87e96dc3ca7
gumemura <guilherme.umemura@looplex.com.br>
2021-02-18 15:50:12 -0300

    gerador de nfe

--- 2021/nfe.py
+++ 2021/nfe.py
@@ -3,22 +3,32 @@ import xlrd
 f = open("items.xml", "w")
 
 # Give the location of the file
-loc = ("estoque.xlsx")
+loc = ("Estoque VRE 2021.xlsx")
  
 # To open Workbook
 wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(0)
+sheet = wb.sheet_by_name("Saida")
 
 num_rows= sheet.nrows
 
 products = []
+i = 0
+while i < 10:
+    if sheet.cell_value(1, i) != "":
+        lastCol = i
+    i = i + 1
 
-for r in range(0, num_rows):
-    product = [sheet.cell_value(r, 0), sheet.cell_value(r, 1)]  
-    products.append(product)
+i = 0
 
-for elem in products:
-    print("Name: " + elem[0] + "\t| " + "Price: " + str(elem[1]))
-    f.write(elem[0] + " " + str(elem[1]) + "\n")
+for i in range(2, num_rows):
+    if sheet.cell_value(i, 1) == 0:
+        break;
+    else:
+        if sheet.cell_value(i, lastCol) != "":
+            print(sheet.cell_value(i, 0) + "\t\t\t" + str(sheet.cell_value(i, lastCol)))
+
+'''for elem in products:
+    print("Name: " + elem[0] + "\t\t| " + "Price: " + str(elem[1]))
+    f.write(elem[0] + " " + str(elem[1]) + "\n")'''
 
 f.close()
